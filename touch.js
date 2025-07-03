cd05be6efd9cb5f253171e6128c9dae2d8d29794
Shivam <p.shivam1097@gmail.com>
2025-07-03 20:47:45 +0530

    added commit

--- touch.js
+++ touch.js
@@ -3,6 +3,7 @@ function addTodo(item) {
   todos.push(item);
   console.log("Todo Added:", item);
   console.log("file added");
+  console.log("delete file");
 }
 addTodo("Learn Git");
 addTodo("Create Git Hands-on Project");
