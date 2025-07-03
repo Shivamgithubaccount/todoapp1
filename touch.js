67e6eb478d63a52917ff50a70d1a28a7d23fd289
Shivam <p.shivam1097@gmail.com>
2025-07-03 20:34:00 +0530

    git branch

---
+++ touch.js
@@ -0,0 +1,8 @@
+let todos = [];
+function addTodo(item) {
+  todos.push(item);
+  console.log("Todo Added:", item);
+  console.log("file added");
+}
+addTodo("Learn Git");
+addTodo("Create Git Hands-on Project");
